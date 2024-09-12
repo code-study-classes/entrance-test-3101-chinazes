822ca62fc1b3ad8915b7286bcb0f67520ea42ed7
mag <polina.arutyunyan.08@mail.ru>
2024-09-12 11:30:52 +0300

    display rooms states

--- src/avaibleRooms.js
+++ src/avaibleRooms.js
@@ -21,12 +21,43 @@ export default () => {
     state.hotelNameToCheck = value;
   });
   
+  const displayObjectContent = (obj, parent) => {
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const value = obj[key];
+            const element = document.createElement('div');
+            
+            if (typeof value === 'object' && value !== null) {
+                const subElement = document.createElement('div');
+                subElement.style.marginLeft = '20px';
+                element.textContent = `${key}:`;
+                displayObjectContent(value, subElement);
+                element.appendChild(subElement);
+            } else {
+                element.textContent = `${key}: ${value}`;
+            }
+
+            parent.appendChild(element);
+        }
+    }
+  };
+
   const addAvaibleRoomsToUlList = (list) => {
     elements.avaibleRooms.innerHTML = '';
     
     list.forEach((room) => {
+      const roomState = state.rooms[room];
+      
+      const currentHotelState = (Object.entries(state.hotels).find(([key]) => key === state.currentHotelName));
+      const region = currentHotelState[1].region;
+      roomState.region = region;
+
       const liEl = document.createElement('li');
+      const contentDiv = document.createElement('div');
+      displayObjectContent(roomState, contentDiv);
       liEl.textContent = room;
+      liEl.classList.add('avaibleListEl');
+      liEl.append(contentDiv);
       elements.avaibleRooms.append(liEl);
     });
   };
