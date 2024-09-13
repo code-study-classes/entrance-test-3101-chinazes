e5d32ac3aad418060c8f0dc50f4a9fdbf1a77b15
mag <polina.arutyunyan.08@mail.ru>
2024-09-13 11:55:47 +0300

    reinstall linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
 export default [
-  {
-      rules: {
-          "no-unused-vars": "error",
-          "no-undef": "error"
-      }
-  }
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
 ];
